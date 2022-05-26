d7575669784c1cdd690fe045ad870beedd7734a8
Ayush Shaw <ayushshaw127@gmail.com>
2022-05-26 08:47:14 +0530

    initial working commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const path=require('path')
+let question = require(path.join(__dirname,'questions.js'))
+const res = require('express/lib/response');
+let answers = ['a', 'b', 'c', 'a', 'c', 'a', 'd', 'a', 'b', 'd', 'b', 'c', 'd', 'a', 'd']
+const app = express()
+app.use('/static', express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname,'home.html'))
+})
+app.post('/question/:qid', (req, res) => {
+    try {
+        let id = parseInt(req.params.qid)
+        if (id < 0 || id >= answers.length) {
+            res.status(404).send(JSON.stringify({ "evaluation": "undefined" }));
+            res.end();
+        } else {
+            let json = req.body
+            // console.log(json)
+            if (json.answer == answers[id]) {
+                res.send(JSON.stringify({ "evaluation": "correct" }));
+                res.end()
+            } else {
+                res.send(JSON.stringify({ "evaluation": "incorrect" }));
+                res.end();
+            }
+        }
+    } catch (error) {
+        res.send(JSON.stringify({ "evaluation": "undefined" }), 404);
+        res.end();
+    }
+})
+app.get('/getQ/:qid', (req, res)=>{
+    try{
+        let id=parseInt(req.params.qid)
+        if(id<0 || id>=answers.length){
+            res.status(404).send(JSON.stringify({"question": "unavailable"}))
+            res.end()
+        }else{
+            res.send(JSON.stringify(question[id]))
+            res.end();
+        }
+    }catch(err){
+        res.status(403).send(JSON.stringify({"question": "unavailable"}))
+        res.end()
+    }
+})
+app.all("*",(req, res)=>{
+    res.status(404).send(JSON.stringify({"evaluation": "undefined"}))
+    res.end()
+})
+app.listen(3000, () => {
+    console.log('App running in localhost 3000')
+})
